b6daf3a65c246fed3e320dfbc7d1c816a85ecda1
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-06-17 22:45:22 +0530

    Loop

---
+++ Lecture8.java
@@ -0,0 +1,34 @@
+public class Lecture8 {
+    // Loop
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 0; i < 100; i++) {
+            System.out.println(i);
+
+        }
+        System.out.println("The value of i = " + i);
+        // While loop
+
+        int n = 5;
+        while (n > 0) {
+            System.out.println("Ritik");
+            n--;
+
+        }
+
+        // do while loop
+        int m = 5;
+        do {
+            System.out.println("Zooooom");
+            m--;
+        } while (m > 0);
+
+        int k = 0;
+        do {
+            System.out.println("Hello");
+            k--;
+        } while (k > 0);
+
+    }
+}
